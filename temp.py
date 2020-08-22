5c9e852c9acb95d3af1bb651b0f68cdc0bb06c1b
shamEiNew <panchalshamsundar7@gmail.com>
2020-08-22 12:56:04 +0530

    done

--- temp.py
+++ temp.py
@@ -11,17 +11,13 @@ def check_mentions(api, since_id):
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
-        since_id=since_id).items(10):
+        since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
         if tweet.in_reply_to_status_id is not None:
             continue
-        #print(prev_tweets(api, api.get_status(tweet.id).screen_name, tweet.id))
         if api.me().screen_name not in prev_tweets(api, api.get_status(tweet.id).user.screen_name, str(tweet.id)):
             logger.info(f"Answering to {tweet.user.name}")
-            api.update_status(
-                status="u are absolutely amazing \U0001F970",
-                in_reply_to_status_id=tweet.id, auto_populate_reply_metadata = True
-                )
+            api.update_status(status="u are absolutely amazing \U0001F970", in_reply_to_status_id=tweet.id, auto_populate_reply_metadata = True)
     return new_since_id
 
 def prev_tweets(api, name, tweet_id):
@@ -30,13 +26,11 @@ def prev_tweets(api, name, tweet_id):
         #print(type(tweet))
         if hasattr(tweet, 'in_reply_to_status_id_str'):
             if (tweet.in_reply_to_status_id_str==tweet_id):
-                status = api.get_status(int(tweet_id))
-                #print(status)
+                status = api.get_status(int(tweet.id))
                 s.append(status.user.screen_name)
-                print(status.user.screen_name)
-                #user_name_tweet(s)
+    if not s:
+        return [0,]
     return s
-    #return s
 
 def mentions_main():
     api = create_api()
