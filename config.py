2141b13da7531ef670beb39dc6118e694c29de5c
shamEiNew <panchalshamsundar7@gmail.com>
2020-08-23 18:49:35 +0530

    changed confug

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from os import environ
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = environ[API_KEY]
-    consumer_secret = environ[API_SECRET_KEY]
-    access_token = environ[ACCESS_TOKEN]
-    access_token_secret = environ[ACCESS_TOKEN_SECRET]
+    consumer_key = environ['API_KEY']
+    consumer_secret = environ['API_SECRET_KEY']
+    access_token = environ['ACCESS_TOKEN']
+    access_token_secret = environ['ACCESS_TOKEN_SECRET']
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
