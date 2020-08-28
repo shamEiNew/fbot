6cd3dadaaeaa73b48c32b407ce6904e070be0c5c
shamEiNew <panchalshamsundar7@gmail.com>
2020-08-28 16:07:24 +0530

    added spotipy

--- fbot.py
+++ fbot.py
@@ -4,6 +4,7 @@ import time
 from os import environ
 import random as rn
 import datetime
+import music_recommend as mr
 
 
 logging.basicConfig(level=logging.INFO)
@@ -27,11 +28,6 @@ except Exception as e:
 logger.info("API created")
 
 def check_mentions(api, keywords, since_id, T):
-    try:
-        quotes = open("quotes.txt", "r", encoding='utf-8')
-    except FileNotFoundError as fr:
-        logger.error("File Doesn't Exist")
-        raise fr
     logger.info("Retrieving mentions")
     new_since_id = since_id
     for tweet in tweepy.Cursor(api.mentions_timeline,
@@ -43,46 +39,48 @@ def check_mentions(api, keywords, since_id, T):
         #    print('Already Liked')
         new_since_id = max(tweet.id, new_since_id)
         if follow_followers(api, api.get_status(tweet.id).user.screen_name) == True:
-            m = api.get_status(tweet.id).user.screen_name
             try:
                 logger.info(f"following {tweet.user.name}")
-                api.create_friendship(m)
+                api.create_friendship(api.get_status(tweet.id).user.screen_name)
             except tweepy.TweepError:
                 logger.error(f"you can't follow {tweet.user.name}!")
 
         if tweet.in_reply_to_status_id is not None:
             continue
-        if api.me().screen_name not in prev_tweets(api, api.get_status(tweet.id).user.screen_name, str(tweet.id)):
-            if api.get_status(tweet.id).user.id_str != environ['MY_USER']:
+        if api.me().screen_name not in prev_tweets(api,
+         api.get_status(tweet.id).user.screen_name, str(tweet.id)):
+            if api.get_status(tweet.id).user.id_str != '1281216898991108097':
                 logger.info(f"Answering to {tweet.user.name}")
                 if any(keyword in tweet.text.lower() for keyword in keywords) == False:
                     try:
                         T += 1
-                        api.update_status(status=str(T) + "Humpty dumpty",
+                        api.update_status(status=str(T) + " Humpty dumpty",
                         in_reply_to_status_id=tweet.id, auto_populate_reply_metadata = True)
                     except tweepy.TweepError:
                         logger.error(f"status duplicate ---01")
                 else:
                     try:
                         T += 1
-                        api.update_status(status=str(T)+": "+rej[rn.randint(0, len(rej))]  , in_reply_to_status_id=tweet.id, auto_populate_reply_metadata = True)
+                        api.update_status(status=str(T)+":sowwwwyyy I give songs to only my fav person \U0001F605 yikes!!!"+"  "+ rej[rn.randint(0, len(rej)-1)]  , in_reply_to_status_id=tweet.id, auto_populate_reply_metadata = True)
                     except:
                         logger.error(f"status duplicate ---02")
             else:
-                
-                logger.info(f"Answering to my user")
-                try:
-                    T += 1
-                    api.update_status(status= "Hi! yup you're my crush. " + quotes.readlines()[rn.randint(0, len(quotes.readlines()))-1],
-                    in_reply_to_status_id=tweet.id,auto_populate_reply_metadata = True)
-                except tweepy.TweepError:
-                    logger.error(f"status duplicate ---03")
-        m = ''
-    try:
-        quotes.close()
-    except:
-        print('file not found')
-
+                if "\U00002764" in tweet.text and len(tweet.text.replace('@TheCrushBot','').replace('\U00002764','').strip())>0:
+                    try:
+                        logger.info(f"Answering to my user: a music")
+                        api.update_status(status = "A song for you {name} \n {song_link}".format(name=tweet.user.name,
+                        song_link = mr.song_pub(tweet.text.replace('@TheCrushBot','').replace('\U00002764','').strip())),
+                        in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
+                    except:
+                        logger.info(f'Status duplicate ---03')
+                else:
+                    try:
+                        logger.info(f"Answering to my user: a reply")
+                        T += 1
+                        api.update_status(status= "luv u \U00002764" + quotes_list[rn.randint(0, len(quotes_list)-1)],
+                        in_reply_to_status_id=tweet.id,auto_populate_reply_metadata = True)
+                    except tweepy.TweepError:
+                        logger.error(f"status duplicate ---04")
     return new_since_id, T
 
 def prev_tweets(api, name, tweet_id):
@@ -107,14 +105,14 @@ def mentions_main():
     while True:
         since_id, T = check_mentions(api, ['are you my crush','you my crush','crush','who is your crush','crushh', 'whos your crush'],since_id, T)
         logger.info("Waiting...")
-        time.sleep(60*60*5)
+        time.sleep(60*60*12)
 
 if (__name__ == "__main__"):
-    rej = ["you beautiful but no \U0001F60D", "may be next time \U0000263A", "There can be only one, sorry",
-     "Nope", "Noooope","Hi! but nayyy","nayyyyyy", "I would have but one at a time \U0001F92A"]
+    rej = ["you beautiful but no \U0001F60D", "may be next time \U0000263A", "There can be only one, sorry","Nope", "Noooope","Hi but nayyy","nayyyyyy", "I would have but one at a time \U0001F92A"]
     try:
-        api.update_status("(^_^) still single now too......(^_^) "+ str(rn.randint(1, 500)))
-    except:
-        logger.info('Status duplicate ---04')
-    
+        quotes = open("fbot/quotes.txt", "r", encoding='utf-8')
+    except FileNotFoundError as fr:
+        logger.error("File Doesn't Exist")
+        raise fr
+    quotes_list = quotes.readlines()
     mentions_main()
---
+++ music_recommend.py
@@ -0,0 +1,27 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import random
+from os import environ
+
+def configure():
+    cs_id = environ['CLIENT_ID']
+    cs_secret = environ['CLIENT_SECRET']
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=cs_id, client_secret=cs_secret)
+    return spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+def song_pub(search_input):
+    sp = configure()
+    results = sp.search(q=search_input.strip().replace(' ','%20'), type='track', market='US' and 'IN',limit = 50)
+     #Replace any space with '%20' or '+'
+    print(search_input.strip().replace(' ','%20'))
+    songs = results['tracks']['items']     #This is list.
+    
+    songs_f = {}                         #Definig dict for our list of songs with names and links.
+    print(songs_f)
+    for i in range(0, len(songs)):
+        songs_f[songs[i]['name']]=songs[i]['external_urls']['spotify']
+    print(songs_f)
+    return random.choice(list(songs_f.values()))
+
+print(song_pub("kasooor"))
