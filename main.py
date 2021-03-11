0fd4de6154a9de3e6311568cf6157424a1ff6ba3
shamEiNew <panchalshamsundar7@gmail.com>
2021-03-11 14:38:07 +0530

    redirected changes

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import time
 import reply_bro as rb
+import config
 
 def main():
+    api = config.create_api()
+    user = 67611162
+    since_id = 1
     while True:
-        sleep_time = 60*60*3
-        rb.reply_bro()
+        sleep_time = 1
+        since_id = rb.reply_bro(api, since_id, user)
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
--- reply_bro.py
+++ reply_bro.py
@@ -2,17 +2,18 @@ import tweepy
 import logging
 from os import environ
 import time
-import config
 
-
-def reply_bro():
-    api = config.create_api()
-    for tweet in tweepy.Cursor(api.user_timeline, user_id = 67611162).items(2):
-        try:
-            api.update_status(
-                status = tweet.text.encode('ascii','ignore').decode('utf-8') + " bro",
-                in_reply_to_status_id=tweet.id,
-                auto_populate_reply_metadata = True
-                )
-        except:
-            pass
+def reply_bro(api, since_id, user):
+    max_id = since_id
+    for tweet in tweepy.Cursor(api.user_timeline, user_id = user, since_id = since_id).items(2):
+        max_id = max(since_id, tweet.id)
+        if tweet.in_reply_to_status_id is None:
+            try:
+                api.update_status(
+                    status = tweet.text.encode('ascii','ignore').decode('utf-8') + " bro",
+                    in_reply_to_status_id=tweet.id,
+                    auto_populate_reply_metadata = True
+                    )
+            except:
+                pass
+    return max_id
