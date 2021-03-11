2ea112d4c5c0fdfba97029dd6360d2dd233a3c7a
shamEiNew <panchalshamsundar7@gmail.com>
2021-03-11 19:00:00 +0530

    added logger info

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import time
+import logging
 import reply_bro as rb
 import config
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 def main():
     api = config.create_api()
     user = 67611162
-    since_id = 1
+    since_id = 1369974514088439810
     while True:
         sleep_time = 60*60*2
         since_id = rb.reply_bro(api, since_id, user)
+        logger.info(f"{since_id}")
+        logger.info("sleeping...")
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
--- reply_bro.py
+++ reply_bro.py
@@ -3,17 +3,30 @@ import logging
 from os import environ
 import time
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
 def reply_bro(api, since_id, user):
     max_id = since_id
-    for tweet in tweepy.Cursor(api.user_timeline, user_id = user, since_id = since_id).items(2):
+    for tweet in tweepy.Cursor(
+        api.user_timeline,
+        user_id = user,
+        since_id = since_id).items():
+
         max_id = max(since_id, tweet.id)
+
         if tweet.in_reply_to_status_id is None:
+
             try:
+
                 api.update_status(
                     status = tweet.text.encode('ascii','ignore').decode('utf-8') + " bro",
                     in_reply_to_status_id=tweet.id,
                     auto_populate_reply_metadata = True
                     )
+                logger.info("replied to dimension boi")
+
             except:
                 pass
+
     return max_id
